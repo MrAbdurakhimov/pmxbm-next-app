457babca275ae67cd0c95d7be55e4ef541a90552
Mukhammadyusuf Abdurakhimov <mukhammadyusuf_abdurakhimov@epam.com>
2022-03-26 04:30:49 +0500

    Mobile menu

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,9 +1,10 @@
 import Link from 'next/link';
-import React from 'react';
-import { Menu } from 'react-feather';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 import theme from '../config/theme';
-import RoundedButton from './elements/RoundedButton';
+import { RoundedButton } from './elements/';
+import { Menu } from 'react-feather';
+import { tablet, mobile } from './utils/responsive';
 const Container = styled.div`
   display: flex;
   align-items: center;
@@ -22,50 +23,111 @@ const MainMenu = styled.div`
   display: flex;
   align-items: center;
   justify-content: space-around;
+  ${tablet({
+    display: 'none',
+  })}
 `;
+
+const ToggleButton = styled.div`
+  display: none;
+  ${tablet({
+    display: 'block',
+  })}
+`;
+
+const MobileMenu = styled.div`
+  transition: all 0.3s ease-in-out;
+  background-color: ${theme.colors.primary + '96'};
+  color: ${theme.colors.white};
+  padding: 8% 42px;
+  display: none;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-around;
+  height: 64vh;
+  font-size: 20px;
+  font-weight: bold;
+  ${tablet({
+    display: 'flex',
+  })}
+`;
+
 const Item = styled.div``;
 
 const Header = () => {
+  const [isOpen, setIsOpen] = useState(false);
   return (
-    <Container>
-      <Logo>Logo</Logo>
-      <MainMenu>
-        <Item>
-          <Link href={'/'}>
-            <a>Additional Service</a>
-          </Link>
-        </Item>
-        <Item>
-          <Link href={'/'}>
-            <a>Teacher Recource</a>
-          </Link>
-        </Item>
-        <Item>
-          <Link href={'/'}>
-            <a>Events</a>
-          </Link>
-        </Item>
-        <Item>
-          <Link href={'/'}>
-            <a>Awards Nomination</a>
-          </Link>
-        </Item>
-        <Item>
-          <Link href={'/'}>
-            <a>Contact us</a>
-          </Link>
-        </Item>
-        <Item>
-          <Link href={'/'}>
-            <a>
-              <RoundedButton background={theme.colors.white}>
-                <Menu color={theme.colors.white} />
-              </RoundedButton>
-            </a>
-          </Link>
-        </Item>
-      </MainMenu>
-    </Container>
+    <>
+      <Container>
+        <Logo>Logo</Logo>
+        <MainMenu>
+          <Item>
+            <Link href={'/'}>
+              <a>Additional Service</a>
+            </Link>
+          </Item>
+          <Item>
+            <Link href={'/'}>
+              <a>Teacher Recource</a>
+            </Link>
+          </Item>
+          <Item>
+            <Link href={'/'}>
+              <a>Events</a>
+            </Link>
+          </Item>
+          <Item>
+            <Link href={'/'}>
+              <a>Awards Nomination</a>
+            </Link>
+          </Item>
+          <Item>
+            <Link href={'/'}>
+              <a>Contact us</a>
+            </Link>
+          </Item>
+        </MainMenu>
+        <ToggleButton
+          onClick={() => {
+            setIsOpen(!isOpen);
+            console.log(isOpen);
+          }}
+        >
+          <RoundedButton background={theme.colors.white}>
+            <Menu color={theme.colors.white} />
+          </RoundedButton>
+        </ToggleButton>
+      </Container>
+      {isOpen && (
+        <MobileMenu>
+          <Item>
+            <Link href={'/'}>
+              <a>Additional Service</a>
+            </Link>
+          </Item>
+          <Item>
+            <Link href={'/'}>
+              <a>Teacher Recource</a>
+            </Link>
+          </Item>
+          <Item>
+            <Link href={'/'}>
+              <a>Events</a>
+            </Link>
+          </Item>
+          <Item>
+            <Link href={'/'}>
+              <a>Awards Nomination</a>
+            </Link>
+          </Item>
+          <Item>
+            <Link href={'/'}>
+              <a>Contact us</a>
+            </Link>
+          </Item>
+        </MobileMenu>
+      )}
+    </>
   );
 };
 
