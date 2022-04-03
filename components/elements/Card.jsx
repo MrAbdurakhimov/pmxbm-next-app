0f1e956757750cdbad2396fa3c044bd658d60a10
Mukhammadyusuf Abdurakhimov <icoderx@yandex.ru>
2022-04-03 09:54:53 +0500

    update

--- components/elements/Card.jsx
+++ components/elements/Card.jsx
@@ -4,11 +4,13 @@ import { Button } from '../elements';
 import theme from '../../config/theme';
 import Link from 'next/link';
 import { tablet } from '../utils/responsive';
+import Image from 'next/image';
 
 const Container = styled.div`
   transition: 0.2s all ease-in-out;
-  padding: 24px;
-  background-color: #f5faff;
+  padding: 82px 24px 24px 24px;
+  background-color: ${theme.colors.primary};
+  color: ${theme.colors.white};
   flex-grow: 1;
   display: flex;
   justify-content: space-between;
@@ -24,11 +26,13 @@ const Container = styled.div`
   })}
   &:hover {
     transition: 0.2s all ease-in-out;
-    box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.2);
-    background-color: ${theme.colors.primary};
-    color: ${theme.colors.white};
+    box-shadow: 0px 0px 24px rgba(0, 0, 0, 0.1);
+    background-color: ${theme.colors.white};
+    color: ${theme.colors.primary};
+
     cursor: pointer;
   }
+  position: relative;
 `;
 
 const Heading = styled.div`
@@ -43,9 +47,25 @@ const Description = styled.div`
   width: 100%;
 `;
 
+const Circle = styled.div`
+  width: 120px;
+  height: 120px;
+  border-radius: 50%;
+  background-color: ${theme.colors.primary};
+  position: absolute;
+  top: -60px;
+  margin-bottom: 60px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
 const Card = ({ title, description, buttonUrl, buttonText }) => {
   return (
     <Container>
+      <Circle>
+        <Image alt={'Card'} src={'/icons/hr.png'} width={54} height={54} />
+      </Circle>
       <Heading>{title}</Heading>
       <Description>{description}</Description>
       <Link href={buttonUrl || '/'}>
